d0b4c362385c781fc30c1c56933c7fac4a1d5b44
sukang <skcourage@163.com>
2018-12-05 15:56:40 +0800

    enable async

--- sd-core/src/main/java/com/SdCoreApplication.java
+++ sd-core/src/main/java/com/SdCoreApplication.java
@@ -3,7 +3,9 @@ package com;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author sukang
@@ -12,6 +14,8 @@ import org.springframework.context.annotation.ComponentScan;
 		scanBasePackages = {"com"}
 
 		)
+@EnableConfigurationProperties
+@EnableAsync
 public class SdCoreApplication {
 
 	public static void main(String[] args) {
