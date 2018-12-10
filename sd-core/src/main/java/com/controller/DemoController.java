a8c8966c5ec71ee0f54bc80fe4245f6953bb8559
sukang <skcourage@163.com>
2018-12-10 16:14:47 +0800

    test修改

--- sd-core/src/main/java/com/controller/DemoController.java
+++ sd-core/src/main/java/com/controller/DemoController.java
@@ -2,11 +2,9 @@ package com.controller;
 
 import com.common.dto.ResponseBean;
 import com.common.dto.StatusInfo;
-import com.common.exception.BusinessException;
 import com.common.exception.Exceptions;
 import com.controller.core.BaseController;
 import net.sf.json.JSONObject;
-import org.apache.shiro.event.EventBus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -17,7 +15,6 @@ import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.swing.plaf.basic.BasicRadioButtonMenuItemUI;
 import java.util.Map;
 
 /**
--- sd-core/src/test/java/sd/core/SdCoreApplicationTests.java
+++ sd-core/src/test/java/sd/core/SdCoreApplicationTests.java
@@ -1,30 +1,19 @@
 package sd.core;
 
-import com.HelloAdvice;
-import com.HelloService;
 import com.SdCoreApplication;
 import com.common.entity.ScheduleJobEntity;
 import com.common.util.BeanUtil;
 import com.component.ApplicationUtils;
-import com.mapper.ScheduleJobMapper;
 import com.service.ScheduleJobService;
-import com.sun.org.apache.xpath.internal.SourceTree;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.aop.MethodBeforeAdvice;
-import org.springframework.aop.framework.ProxyFactory;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.lang.Nullable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import javax.inject.Inject;
 import javax.sql.DataSource;
-import java.lang.reflect.Method;
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {SdCoreApplication.class})
