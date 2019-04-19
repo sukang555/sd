946eb6122ba1a1542dda233c2245d95f9fa3e230
sukang <skcourage@163.com>
2019-04-19 10:08:09 +0800

    mybatis

--- sd-config/src/main/java/com/datasource/MybatisConfig.java
+++ sd-config/src/main/java/com/datasource/MybatisConfig.java
@@ -1,18 +1,14 @@
 package com.datasource;
-
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.inject.Named;
 import javax.sql.DataSource;
-import java.util.Arrays;
 
 /**
  * @author sukang on 2018/12/29.
@@ -39,7 +35,6 @@ public class MybatisConfig {
     public SqlSessionTemplate getSqlSessionTemplate(
             @Named("mySqlSessionFactory") SqlSessionFactory sqlSessionFactory){
 
-        SqlSessionTemplate sqlSessionTemplate = new SqlSessionTemplate(sqlSessionFactory);
-        return sqlSessionTemplate;
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
