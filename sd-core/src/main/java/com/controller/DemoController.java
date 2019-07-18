34ebcb0248d2767594c3118287950a75f12a5bed
sukang <skcourage@163.c0m>
2019-07-18 15:17:52 +0800

    branch test

--- sd-core/src/main/java/com/controller/DemoController.java
+++ sd-core/src/main/java/com/controller/DemoController.java
@@ -47,7 +47,7 @@ public class DemoController extends BaseController {
 
     @PostMapping("/success")
     public ResponseBean success(Map<String,String> map){
-        logger.info("master"+BeanUtil.toJsonStr(map));
+        logger.info("master1.0"+BeanUtil.toJsonStr(map));
         return ResponseBean.ok("/feature/test/1.1");
     }
 
