4c582cff5c60014588d7f9e050c257da8f86054f
sukang <sukang555@outlook.com>
2020-09-08 11:45:00 +0800

    密码修改

--- sd-core/src/main/java/com/mapper/EncryptLogMapper.java
+++ sd-core/src/main/java/com/mapper/EncryptLogMapper.java
@@ -6,15 +6,11 @@ import com.common.entity.EncryptLog;
  * @author sukang
  */
 public interface EncryptLogMapper {
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(EncryptLog record);
 
-    int insertSelective(EncryptLog record);
-
     EncryptLog selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(EncryptLog record);
-
-    int updateByPrimaryKey(EncryptLog record);
 }
