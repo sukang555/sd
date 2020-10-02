69ca0014cc9f728fd6a59c207bdca3df39fbac57
sukang <sukang555@outlook.com>
2020-10-02 12:37:18 +0800

    脚本

--- sd-core/bin/start.sh
+++ sd-core/bin/start.sh
@@ -3,7 +3,7 @@
 PROJECT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd | sed 's/\/bin//' )
 SHELL_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )
 source ${SHELL_DIR}/JAVA_ENV.sh
-WEB_PATH=${PROJECT_DIR}/sd-core/target/sd-core-1.0.0.jar
+WEB_PATH=${PROJECT_DIR}/target/sd-core-1.0.0.jar
 echo "PROJECT_DIR is ${PROJECT_DIR}"
 echo "SHELL_DIR is ${SHELL_DIR}"
 
@@ -13,6 +13,8 @@ JAVA_OPT="${JAVA_OPT} -XX:+UseG1GC"
 JAVA_OPT="${JAVA_OPT} -XX:MaxGCPauseMillis=100"
 JAVA_OPT="${JAVA_OPT} -XX:MaxMetaspaceSize=512m"
 JAVA_OPT="${JAVA_OPT} -XX:MetaspaceSize=512m"
+
+
 
 JAVA_FINAL_OPT="${JAVA_MEM_OPTS} ${JAVA_OPT}"
 
