b9a2b73ced461c293ee046374a9a2a903817bdbf
sukang <sukang555@outlook.com>
2020-10-10 15:13:37 +0800

    修改打包启动方式

--- sd-core/bin/JAVA_ENV.sh
+++ sd-core/bin/JAVA_ENV.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-export JAVA_MEM_OPTS="-Xms512m -Xmx1g -Xss256k"
+export JAVA_MEM_OPTS="-Xms512m -Xmx1g -Xss256k -server "
 
-export APPLICATION_MAIN="sd-core-"
+export APPLICATION_MAIN="com.SdCoreApplication"
+
+echo "JAVA_HOME: ${JAVA_HOME}"
+
+export JAVA_HOME
+CLASSPATH="${JAVA_HOME}/lib/tools.jar:${JAVA_HOME}/lib/dt.jar"
+export CLASSPATH
+
+echo "CLASSPATH: ${CLASSPATH}"
--- sd-core/bin/start.sh
+++ sd-core/bin/start.sh
@@ -7,18 +7,35 @@ WEB_PATH=${PROJECT_DIR}/target/sd-core-1.0.0.jar
 echo "PROJECT_DIR is ${PROJECT_DIR}"
 echo "SHELL_DIR is ${SHELL_DIR}"
 
-##配置
+LOG_DIR=${PROJECT_DIR}/logs
+
 JAVA_OPT=""
+if [[ $1 == "" ]]; then
+    JAVA_OPT="-Dspring.profiles.active=dev"
+else
+    JAVA_OPT="-Dspring.profiles.active=${1}"
+fi
+
+## jvm配置
+
 JAVA_OPT="${JAVA_OPT} -XX:+UseG1GC"
 JAVA_OPT="${JAVA_OPT} -XX:MaxGCPauseMillis=100"
 JAVA_OPT="${JAVA_OPT} -XX:MaxMetaspaceSize=512m"
 JAVA_OPT="${JAVA_OPT} -XX:MetaspaceSize=512m"
-
-
+JAVA_OPT="${JAVA_OPT} -Duser.timezone=GMT+8 -XX:+PrintCommandLineFlags"
+JAVA_OPT="${JAVA_OPT} -Xloggc:${PROJECT_DIR}/logs/gc.log"
+JAVA_OPT="${JAVA_OPT} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${PROJECT_DIR}/logs"
+JAVA_OPT="${JAVA_OPT} -Dlogging.config.location=${PROJECT_DIR}/config/logback.xml"
+JAVA_OPT="${JAVA_OPT} -Dspring.config.location=${PROJECT_DIR}/config/application.properties"
 
 JAVA_FINAL_OPT="${JAVA_MEM_OPTS} ${JAVA_OPT}"
 
-echo "${JAVA_FINAL_OPT}"
+CLASSPATH="${CLASSPATH}:${PROJECT_DIR}/config:${PROJECT_DIR}/lib/*"
+
+echo "当前启动项目的环境为： ${1}"
+echo "项目启动的参数为： ${JAVA_FINAL_OPT}"
+echo "classpath路径为：${CLASSPATH}"
+
 
 PID=""
 function getPid() {
@@ -27,10 +44,13 @@ function getPid() {
 
 getPid
 
+
+## 执行主命令  java -cp [classPath] [mainClass]
 if [[ ${PID} != "" ]]; then
     echo "${APPLICATION_MAIN} already started (PID=${PID})"
 else
-    java -jar ${WEB_PATH} &
+    nohup ${JAVA_HOME}/bin/java ${JAVA_FINAL_OPT} -cp ${CLASSPATH} ${APPLICATION_MAIN} > ${LOG_DIR}/start.out 2>&1 &
+    tail -f "${LOG_DIR}/start.out"
 fi
 
 
